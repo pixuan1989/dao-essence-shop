bb06ff8651a5e2219d406c35e6ad945b0401592f
pixuan1989 <pixuan1989@outlook.com>
2026-05-20 15:41:13 +0800

    chore(build-blog): 添加百度自动推送JS脚本到文章页模板
    - 在文章页模板的</body>前添加百度官方自动推送脚本
    - 每次用户访问文章页时自动向百度推送URL
    - 无需ICP备案即可使用，绕过配额0限制

--- build-blog.js
+++ build-blog.js
@@ -1411,6 +1411,20 @@ ${FOOTER_HTML}
     <script src="/js/pageview.js" defer></script>
     <script src="/js/tracking.js"></script>
     <script src="/js/i18n-switcher.js" defer></script>
+    <!-- Baidu Auto Push -->
+    <script>
+    (function(){
+        var bp = document.createElement('script');
+        var curProtocol = window.location.protocol.split(':')[0];
+        if (curProtocol === 'https') {
+            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+        } else {
+            bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+        }
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(bp, s);
+    })();
+    </script>
 </body>
 </html>`;
 }
